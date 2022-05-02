1a9972d9dc4d8df69e89eeaa80ab5cb05c8b3200
PetrCenilov <pilot_master_68@mail.ru>
2022-05-02 11:49:07 +0300

    first commit

---
+++ src/Tank.java
@@ -0,0 +1,57 @@
+public class Tank {
+    int x, y;
+    int dir;
+    int fuel = 100;
+    static int countTank;
+    int c;
+
+    public Tank() {
+        this(0, 0, 100);
+    }
+
+    public Tank(int x, int y) {
+        this(x, y, 100);
+    }
+
+    public Tank(int x, int y, int fuel) {
+        this.x = x;
+        this.y = y;
+        this.fuel = fuel;
+        c = ++countTank;
+    }
+
+    public void move(int i) {
+        if (dir == 0) x += i;
+        else if (dir == 1) y += i;
+        else if (dir == 2) x -= i;
+        else y -= i;
+        fuel = fuel - Math.abs(i);
+    }
+    public void goForward(int i) {
+        if (i < 0 && -i > -fuel)
+            i = -fuel;
+        else if (i > fuel)
+            i = fuel;
+        move(i);
+
+    }
+
+
+    public void printPosition() {
+        System.out.println("The Tank is at " + x +", " + y + " now.");
+    }
+
+    public void turnLeft() {
+        dir--;
+        if (dir == -1) dir = 3;
+    }
+
+    public void turnRight() {
+        dir++;
+        if (dir == 4) dir = 0;
+    }
+
+    public void goBackward(int i) {
+        goForward(-i);
+    }
+}
